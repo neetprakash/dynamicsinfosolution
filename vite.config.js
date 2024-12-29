6ff2042c4d395bfe9c1f697a84ed8445164aa82b
Neet <neet.love1@gmail.com>
2024-12-30 01:37:25 +0530

    deploy changes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import { ghPages } from 'vite-plugin-gh-pages';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
+  base: '/dynamicsinfosolution/', // Replace <repository-name> with your GitHub repo name
+
   css: {
     modules: {
       localsConvention: "camelCase",
